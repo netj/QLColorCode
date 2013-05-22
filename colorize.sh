603011de578c920c5df5bd2c05855eb830a15590
Jaeho Shin <netj@sparcs.org>
2013-05-21 18:21:51 -0700

    Filename in QuickLook title

--- colorize.sh
+++ colorize.sh
@@ -37,6 +37,7 @@ cmd=$hlDir/bin/highlight
 cmdOpts=(-I --font $font --quiet \
          --data-dir $rsrcDir/highlight/share/highlight \
          --add-config-dir $rsrcDir/etc/highlight --style $hlTheme \
+         --doc-title "${target##*/}" \
          --font-size $fontSizePoints --encoding $textEncoding ${=extraHLFlags} --validate-input)
 
 #for o in $cmdOpts; do echo $o\<br/\>; done 
